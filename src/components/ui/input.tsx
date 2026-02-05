cf7ce76f1e0b27e3eb78a4e68b976273a591939f
Subash <subashsuni55@gmail.com>
2026-02-05 11:30:50 +0530

    added shadcn button card field label sidebar etc..

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,14 +1,21 @@
-import { ComponentProps } from "react";
+import * as React from "react"
 
-const Input = ({ className, type, ...props }: ComponentProps<"input">) => {
+import { cn } from "@/lib/utils"
+
+function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
     <input
       type={type}
       data-slot="input"
-      className={`h-9 w-full min-w-0 border bg-transparent px-3 py-1 text-base shadow-xs focus-visible:ring focus-visible:ring-blue-300 focus-visible:outline-0 border-gray-300  ${className}`}
+      className={cn(
+        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+        className
+      )}
       {...props}
     />
-  );
-};
+  )
+}
 
-export default Input;
+export { Input }
