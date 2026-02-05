1ebe53ad642037f68f52be5fa5cfe6e4d97ae1e7
Subash <subashsuni55@gmail.com>
2026-02-05 19:07:55 +0530

    dashboard page

--- src/components/pages/reservation/index.tsx
+++ src/components/pages/reservation/index.tsx
@@ -1,17 +1,18 @@
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Field, FieldLabel } from "@/components/ui/field";
 import InputDate from "@/components/ui/input-date";
 import InputTime from "@/components/ui/input-time";
 
-const ReservationPage = ({ time }: { time: string }) => {
+const ReservationPage = () => {
   return (
     <section className="container mx-auto py-4">
       <h1 className="border-l-5 border-l-yellow-500 pl-4 text-3xl font-semibold">
         Make A Reservation
       </h1>
-      <h1>current time from server : {time}</h1>
       <Card className="mt-4">
-        <CardTitle>Pick your date and time</CardTitle>
+        <CardHeader>
+          <CardTitle>Pick your date and time</CardTitle>
+        </CardHeader>
         <CardContent>
           <form className="flex gap-4">
             <Field>
--- src/routes/reservation/index.tsx
+++ src/routes/reservation/index.tsx
@@ -1,13 +1,13 @@
 import ReservationPage from "@/components/pages/reservation";
-import { getServerTime } from "@/util/functions/getServerTime";
+// import { getServerTime } from "@/util/functions/getServerTime";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/reservation/")({
   component: RouteComponent,
-  loader: async () => await getServerTime(),
+  // loader: async () => await getServerTime(),
 });
 
 function RouteComponent() {
-  const time = Route.useLoaderData();
-  return <ReservationPage time={time} />;
+  // const time = Route.useLoaderData();
+  return <ReservationPage />;
 }
