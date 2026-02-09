b03582490fdc994692fa30ea6ccc68e9772ec2e0
Subash <subashsuni55@gmail.com>
2026-02-09 11:32:05 +0530

    updated  logout

--- src/components/pages/dashboard/index.tsx
+++ src/components/pages/dashboard/index.tsx
@@ -1,38 +1,20 @@
-import { Button } from "@/components/ui/button";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { authClient } from "@/lib/auth-client";
-import { Outlet, useNavigate } from "@tanstack/react-router";
+import { Outlet } from "@tanstack/react-router";
 import AppSidebar from "./app-sidebar";
-import { LogOut } from "lucide-react";
+import Logout from "./logout";
 
 const DashboardPage = () => {
-  const navigate = useNavigate();
-  const handleSignOut = async () => {
-    await authClient.signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          navigate({ to: "/login" });
-        },
-      },
-    });
-  };
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
         <header className="flex items-center justify-between p-4 border-b">
           <SidebarTrigger className="-ml-1" />
-          <Button
-            onClick={handleSignOut}
-            size={"icon"}
-            className="cursor-pointer"
-          >
-            <LogOut />
-          </Button>
+          <Logout />
         </header>
         <main className="p-8">
           <Outlet />
